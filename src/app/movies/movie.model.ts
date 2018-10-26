f48439e1cce214363754c8a9bee4212002adfd81
hppod <hppodgornik@gmail.com>
2018-10-26 11:05:54 -0300

    criando a interface Movie

---
+++ src/app/movies/movie.model.ts
@@ -0,0 +1,11 @@
+export interface Movie {
+    id: string
+    name: string
+    genre: string
+    year: number
+    duration: string
+    studio: string
+    rating: string
+    imagePath: string
+    about: string
+}
