db31df3195a0b93515bdfa1a019545b9241c18bc
hppod <hppodgornik@gmail.com>
2018-10-26 10:53:02 -0300

    criando o componente movies, criando a rota correspondente ao componente, associando a rota criada aos botões correspondentes em header e home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component'
 import { ROUTES } from './app.routes';
+import { MoviesComponent } from './movies/movies.component';
 
 
 @NgModule({
@@ -16,7 +17,8 @@ import { ROUTES } from './app.routes';
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    MoviesComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 import { AboutComponent } from "./about/about.component";
+import { MoviesComponent } from "./movies/movies.component";
 
 export const ROUTES: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'about', component: AboutComponent }
+    { path: 'about', component: AboutComponent },
+    { path: 'movies', component: MoviesComponent }
 ]
