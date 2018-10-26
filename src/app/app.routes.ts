407d8831a8c1e6c5184bda51fefb532df828bda9
hppod <hppodgornik@gmail.com>
2018-10-26 11:31:11 -0300

    criando a rota parametrizada para acessar o componente movie-detail

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 import { AboutComponent } from "./about/about.component";
 import { MoviesComponent } from "./movies/movies.component";
+import { MovieDetailComponent } from "./movie-detail/movie-detail.component";
 
 export const ROUTES: Routes = [
     { path: '', component: HomeComponent },
     { path: 'about', component: AboutComponent },
-    { path: 'movies', component: MoviesComponent }
+    { path: 'movies', component: MoviesComponent },
+    { path: 'movie/:id', component: MovieDetailComponent }
 ]
