a3bf34a9832c3f171961fa0c80592f632ca1d44d
hppod <hppodgornik@gmail.com>
2018-10-27 16:34:50 -0300

    criando a rota filha do componente movie-detail

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,17 @@ import { HomeComponent } from "./home/home.component";
 import { AboutComponent } from "./about/about.component";
 import { MoviesComponent } from "./movies/movies.component";
 import { MovieDetailComponent } from "./movie-detail/movie-detail.component";
+import { ActorsComponent } from "./movie-detail/actors/actors.component";
 
 export const ROUTES: Routes = [
     { path: '', component: HomeComponent },
     { path: 'about', component: AboutComponent },
     { path: 'movies', component: MoviesComponent },
-    { path: 'movie/:id', component: MovieDetailComponent }
+    {
+        path: 'movie/:id', component: MovieDetailComponent,
+        children: [
+            { path: '', redirectTo: 'actors', pathMatch: 'full' },
+            { path: 'actors', component: ActorsComponent }
+        ]
+    }
 ]
